b535142f8adafc5e22b31c0c971eafe01a4351ef
Nat Gist <nat.gist@gmail.com>
2025-10-15 11:24:48 -0400

    change idle detection and add debug messages

--- esphome/components/neptune_water_meter/neptune_water_meter.cpp
+++ esphome/components/neptune_water_meter/neptune_water_meter.cpp
@@ -15,6 +15,7 @@ constexpr size_t MAX_BITS = BUFFER_SIZE * BITS_PER_BYTE;
 void IRAM_ATTR HOT NeptuneWaterMeterSensorStore::clock_interrupt(NeptuneWaterMeterSensorStore *arg) {
   // Capture data as quickly as possible when clock rises
   bool data = arg->pin_data.digital_read();
+  arg->last_bit_time = millis();
   u_int32_t write_index = arg->write_index;
 
   // Stuff the bit in the buffer, reader is responsible for clearing out the buffer after it's read.
@@ -44,6 +45,7 @@ void NeptuneWaterMeterSensor::setup() {
   this->pin_clock_->setup();
   this->pin_data_->setup();
   this->store_.pin_data = this->pin_data_->to_isr();
+  this->store_.last_bit_time = millis();
 
   this->pin_clock_->attach_interrupt(NeptuneWaterMeterSensorStore::clock_interrupt, &this->store_,
                                      gpio::INTERRUPT_RISING_EDGE);
@@ -55,52 +57,56 @@ void NeptuneWaterMeterSensor::dump_config() {
   ESP_LOGCONFIG(TAG, "  Scale Factor: %f.1", this->scale_factor_);
 }
 void NeptuneWaterMeterSensor::loop() {
-  int32_t bits_captured = this->store_.write_index - this->read_index_;
-  if (bits_captured < 0) {
-    bits_captured += MAX_BITS;
-  }  // Deal with ring buffer wrapping around
+  u_int32_t time_since_last_bit = millis() - this->store_.last_bit_time;
+  bool bus_idle = time_since_last_bit > 1000;
+  // Capture volatile value once to maintain a consistent state throughout the loop.
+  u_int32_t write_index = this->store_.write_index;
+  u_int32_t bits_captured = write_index - this->read_index_;
 
   if (!bits_captured) {
     this->disable_loop();
+    ESP_LOGD(TAG, "Loop disabled");
     return;
   }
 
-  // TODO: Find a better way to check for idle for now...
-  // Have bits and they haven't changed, transmission must be completed.
-  bool bus_idle = this->last_bits_captured_ == bits_captured;
-  if (bus_idle && bits_captured) {
-    bool buffer_corrupted = false;
-    ESP_LOGD(TAG, "Captured %d unprocessed bits.");
-    if (bits_captured % BITS_PER_BYTE != 0) {
-      // Bits received should be divisible by 4
-      ESP_LOGW(TAG, "Incomplete Data Received");
-      buffer_corrupted = true;
-    }
-    if (this->read_index_ % BITS_PER_BYTE != 0) {
-      // First bit should be aligned to a byte boundary
-      ESP_LOGE(TAG, "Data Alignment Error");
-      buffer_corrupted = true;
-    }
-    if (buffer_corrupted) {
-      ESP_LOGD(TAG, "Buffer corrupted flushing");
-      this->flush_buffer_();
-      return;
-    }
+  if (!bus_idle) {
+    ESP_LOGD(TAG, "Time since last bit %d", time_since_last_bit)
+    return;
+  }
 
-    int32_t begin = this->read_index_ / BITS_PER_BYTE;
-    int32_t end = (this->read_index_ + bits_captured) / BITS_PER_BYTE;
-    this->raw_message_.fill(0);
-    for (int i = begin; i < end; i++) {
-      this->raw_message_[i - begin] = this->store_.bit_buffer[i % BUFFER_SIZE];
-      ESP_LOGD(TAG, "%s", this->raw_message_.data());
-    }
+  bool buffer_corrupted = false;
+  ESP_LOGD(TAG, "Captured %d unprocessed bits.");
+  if (bits_captured % BITS_PER_BYTE != 0) {
+    // Bits received should be divisible by 4
+    ESP_LOGW(TAG, "Incomplete Data Received");
+    buffer_corrupted = true;
+  }
+  if (this->read_index_ % BITS_PER_BYTE != 0) {
+    // First bit should be aligned to a byte boundary
+    ESP_LOGE(TAG, "Data Alignment Error");
+    buffer_corrupted = true;
+  }
+  if (buffer_corrupted) {
+    ESP_LOGD(TAG, "Buffer corrupted flushing");
+    this->flush_buffer_();
+    return;
+  }
 
-    uint32_t reading = this->parse_reading_();
-    if (this->last_reading_ != reading) {
-      this->last_reading_ = reading;
-      this->publish_state(reading);
-      this->listeners_.call(reading);
-    }
+  int32_t begin = this->read_index_ / BITS_PER_BYTE;
+  int32_t end = (this->read_index_ + bits_captured) / BITS_PER_BYTE;
+  this->raw_message_.fill(0);
+  for (int i = begin; i < end; i++) {
+    this->raw_message_[i - begin] = this->store_.bit_buffer[i % BUFFER_SIZE];
+  }
+  // Advance index
+  this->read_index_ = write_index;
+
+  ESP_LOGD(TAG, "%s", this->raw_message_.data());
+  uint32_t reading = this->parse_reading_();
+  if (this->last_reading_ != reading) {
+    this->last_reading_ = reading;
+    this->publish_state(reading);
+    this->listeners_.call(reading);
   }
 }
 
