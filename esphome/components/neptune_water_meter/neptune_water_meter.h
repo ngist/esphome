118e7dfa519ffd359ed469bf7385b721397a59f4
Nat Gist <nat.gist@gmail.com>
2025-10-24 11:58:59 -0400

    allow config of clock edge

--- esphome/components/neptune_water_meter/neptune_water_meter.h
+++ esphome/components/neptune_water_meter/neptune_water_meter.h
@@ -14,6 +14,11 @@ constexpr size_t BUFFER_SIZE = 512;
 
 class NeptuneWaterMeterSensor;
 
+enum NeptuneWaterMeterSensorClockMode {
+  RISING_EDGE = 0,
+  FALLING_EDGE,
+};
+
 struct NeptuneWaterMeterSensorStorage {
   ISRInternalGPIOPin pin_data;
   ISRInternalGPIOPin pin_clock;
@@ -25,6 +30,7 @@ struct NeptuneWaterMeterSensorStorage {
   volatile uint32_t falling_edge_triggers;
   volatile uint32_t filtered_out_triggers;
   volatile uint32_t last_bit_time;
+  bool clock_falling_edge_;
   std::array<uint16_t, BUFFER_SIZE> bit_buffer;
 
   static void clock_interrupt(NeptuneWaterMeterSensorStorage *arg);
@@ -40,6 +46,7 @@ class NeptuneWaterMeterSensor : public sensor::Sensor, public Component {
    * Not all neptune meters report the same units so some readings need to be multiplied by a scale factor.
    */
   void set_scale_factor(double_t scale_factor) { scale_factor_ = scale_factor; }
+  void set_clock_edge(NeptuneWaterMeterSensorClockMode edge) { storage_.clock_falling_edge_ = edge; }
 
   // ========== INTERNAL METHODS ==========
   // (In most use cases you won't need these)
@@ -58,7 +65,7 @@ class NeptuneWaterMeterSensor : public sensor::Sensor, public Component {
   uint32_t read_index_{0};
   std::array<uint16_t, BUFFER_SIZE> raw_message_{0};
   uint32_t last_reading_{0};
-  double_t scale_factor_;
+  double_t scale_factor_{1};
 
   NeptuneWaterMeterSensorStorage storage_{.water_meter = *this};
 
